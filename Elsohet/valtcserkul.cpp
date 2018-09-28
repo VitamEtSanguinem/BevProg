1c8db9b8c8a9cd076cb724ae198658580f782945
VitamEtSanguinem <43444279+VitamEtSanguinem@users.noreply.github.com>
2018-09-28 15:50:35 +0200

    Add files via upload

---
+++ Elsohet/valtcserkul.cpp
@@ -0,0 +1,16 @@
+#include"std_lib_facilities.h"
+int main(){
+
+int a = 13;
+int b = 21;
+
+cout << a << '\t' << b << '\n';
+
+a=a+b;
+b=a-b;
+a=a-b;
+
+cout << a << '\t' << b << '\n';
+
+
+}
---
+++ Elsohet/valtcserszor.cpp
@@ -0,0 +1,19 @@
+#include"std_lib_facilities.h"
+
+int main(){
+
+int a=9;
+int b=3;
+
+cout << a << '\t' << b << '\n';
+
+a=a*b;
+b=a/b;
+a=a/b;
+
+cout << a << '\t' << b << '\n';
+
+
+
+
+}
