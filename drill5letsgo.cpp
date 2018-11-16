58c1e9ed683225443a176bbbf2bf64161ebee8f1
VitamEtSanguinem <43444279+VitamEtSanguinem@users.noreply.github.com>
2018-11-16 18:59:58 +0100

    Add files via upload

---
+++ drill5letsgo.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+int main(){
+bool(c)=false;
+int x=2000; 
+char c= x;
+int i=0; 
+int j=0;
+int x1=2; 
+double d1=5/(x1-2);
+int x2= 4; 
+double d2= 5/(x2-2);
+
+
+	try {
+		cout <<"Success!\n";
+		cout <<"Success!\n";
+		cout <<"Success!\n";
+		cout <<"Success!\n";
+		string res = 7;std::vector<int> v(10);v[5]=res;cout <<"Success!\n";
+		if(cond) cout<<"Success!\n"; else cout <<"Fail!\n";
+		 if(c) cout << "Success!\n"; else cout<< "Fail!\n";
+		string s ="ape"; bool c = "fool">s; if (c) cout<<"Success!\n";
+		string s ="ape"; if(s=="fool") cout<<"Success!\n";
+		string s ="ape"; if(s=="fool") cout<<"Success!\n";
+		string s ="ape"; if(s!="fool")  cout<<"Success!\n";
+		std::vector<char>v(5);for(int i=0; 0<v.size();++i); cout<<"Success!\n";
+		std::vector<char>v(5);for(int i=0; 0<v.size();++i); cout<<"Success!\n";
+		string s= "Success!\n";for(int i=0; i<6;++i); cout << s[i];
+		if(true)cout<<"Success!\n"; else cout<<"Fail!\n";
+		 if(c=2000) cout << "Success!\n";
+		string s = "Success!\n";int i; for(int i=0; i<10; ++i); cout<<s[i];
+		std::vector<int> v(5); for(int i=0;i<v.size(); ++i); cout << "Success!\n";
+		 while(i<10); ++j; if(j<i) cout <<"Success!\n";
+		
+		 if(d==2*x1+0.5) cout <<"Success!\n";
+		string s1= "Success!\n";int i; for (int i=0; i<=10; ++i); cout << s1[i];
+		 while(i<10); if(j<i) cout <<"Success!\n";
+		 if(d=2*x2+0.5) cout << "Success!\n";
+		cout<<"Success!\n";  
+
+	
+
+}
+
+
+catch(exception&e){
+
+	cerr<<"error << e.what()<< '\n'";
+	return 1;
+}
+
+catch(...){
+
+	cerr<< "Oops, EXCEPTION not found!\n";
+	return 2;
+}
+
+
+
+
+
+}
